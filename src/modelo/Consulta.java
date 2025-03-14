2a6cf116627e67fc71874939b9cdf83b5c2aa943
itallume <itallo.oliver21@gmail.com>
2025-03-14 02:30:29 -0300

    tabelas renomeadas

--- src/modelo/Consulta.java
+++ src/modelo/Consulta.java
@@ -10,9 +10,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;;
 
 @Entity
+@Table(name="consulta20231370014")
 public class Consulta {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/modelo/Paciente.java
+++ src/modelo/Paciente.java
@@ -7,9 +7,11 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 
 @Entity
+@Table(name="paciente20231370014")
 public class Paciente {
 	@Id
 	private String cpf;
